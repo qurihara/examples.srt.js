6bcf8fe6b1b88219239e445ca83a66de50348f3e
qurihara <qurihara@unryu.org>
2017-05-17 14:23:13 +0900

    ok

---
+++ simple/playbackrate.srt.js
@@ -0,0 +1,6 @@
+1
+00:00:00,000 --> 00:00:01,000
+player.pauseVideo();
+var str = prompt("Input playback rate.","2");﻿
+player.setPlaybackRate(str);
+player.playVideo();
---
+++ simple/window_close.srt.js
@@ -0,0 +1,3 @@
+1
+00:02:51,000 --> 00:03:00,000
+window.open('about:blank','_self').close();
