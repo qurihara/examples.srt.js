6074d2dd9bea02643f3e4759aab4e35b11712b8e
qurihara <qurihara@unryu.org>
2018-07-16 20:31:33 +0900

    ok

--- obniz/led.srt.js
+++ obniz/led.srt.js
@@ -10,15 +10,23 @@ led = obniz.wired("LED", { anode:0, cathode:1 } );
 1
 00:00:29,500 --> 00:00:31,500
 led.on();
+obniz.display.clear();
+obniz.display.print("ON");
 
 2
-00:00:34,500 --> 00:00:35,500
+00:00:34,000 --> 00:00:35,500
 led.off();
+obniz.display.clear();
+obniz.display.print("OFF");
 
 3
-00:00:37,400 --> 00:00:38,500
+00:00:37,000 --> 00:00:38,500
 led.on();
+obniz.display.clear();
+obniz.display.print("ON");
 
 4
 00:00:40,500 --> 00:00:41,500
 led.off();
+obniz.display.clear();
+obniz.display.print("OFF");
